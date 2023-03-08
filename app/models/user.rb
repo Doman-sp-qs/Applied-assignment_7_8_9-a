45d9546714fbd4e3a060db0eb9b94208e901cfec
Doman-sp-qs <122662138+Doman-sp-qs@users.noreply.github.com>
2023-03-08 00:31:40 +0000

    [Finish] 応用課題5完了

--- app/models/user.rb
+++ app/models/user.rb
@@ -21,12 +21,12 @@ class User < ApplicationRecord
   validates :introduction, length: {maximum: 50}
 
   
-  
+  # プロフィール画像取得
   def get_profile_image
     (profile_image.attached?) ? profile_image : 'no_image.jpg'
   end
   
-  
+  # フォローフォロワー機能
   def follow(user_id)
     relationships.create(followed_id: user_id)
   end
@@ -39,7 +39,20 @@ class User < ApplicationRecord
     followings.include?(user)
   end
   
-  
+  # 検索方法分岐
+  def self.looks(search, word)
+    if search == "perfect_match"
+      @user = User.where("name LIKE?", "#{word}")
+    elsif search == "forward_match"
+      @user = User.where("name LIKE?","#{word}%")
+    elsif search == "backward_match"
+      @user = User.where("name LIKE?","%#{word}")
+    elsif search == "partial_match"
+      @user = User.where("name LIKE?","%#{word}%")
+    else
+      @user = User.all
+    end
+  end
   
   
 end
